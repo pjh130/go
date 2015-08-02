754a35a0e3ec4e8030e14f47856ff854344f2d8f
pjh130 <pjh130@163.com>
2015-08-02 20:17:37 +0800

    sfs

---
+++ common/weblib/httprouter.go
@@ -0,0 +1,26 @@
+package weblib
+
+//HttpRouter is a lightweight high performance HTTP request router
+//(also called multiplexer or just mux for short) for Go.
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
+
+func ExampleHttprouter() {
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/hello/:name", Hello)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
