e703e8f59c7da91d9d6cf05b8daa05e538149791
pjh130 <pjh130@163.com>
2015-08-31 16:44:40 +0800

    m

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 go
+
+Go语言(golang)开源项目大全
+http://www.open-open.com/lib/view/open1396063913278.html#Configuration_File_Parsers
--- demo/template/main.go
+++ demo/template/main.go
@@ -7,6 +7,12 @@ import (
 	"os"
 )
 
+/*
+Go语言的模板通过{{}}来包含需要在渲染时被替换的字段，{{.}}表示当前的对象，这和Java或者C++中的this类
+似，如果要访问当前对象的字段通过{{.FieldName}},但是需要注意一点：这个字段必须是导出的(字段首字母必
+须是大写的),否则在渲染的时候就会报错
+*/
+
 type Person struct {
 	Name    string
 	Age     int
@@ -43,6 +49,8 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	Test1()
+
 	Test2()
 
 	http.HandleFunc("/", Handler)
